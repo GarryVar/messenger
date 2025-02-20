d4d87bbf8b8b1b7bef45136acd68586a80a412e7
garryvar <darisvet.sokolov@yandex.ru>
2025-02-20 09:56:20 +0300

    Added vite-plugin-webfont-dl to vite.confing.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    webfontDownload([
+      "https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap",
+      "https://fonts.googleapis.com/css2?family=Fira+Code&display=swap",
+      "https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap",
+      "https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap",
+      "https://fonts.googleapis.com/css2?family=Coiny&family=Onest:wght@100..900&display=swap",
+    ]),
+  ],
+});
